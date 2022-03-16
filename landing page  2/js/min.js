6e09ccd875a27c0d2f95d818b9f6c37d1499a45b
shahariyar Nafis <72545687+shahariyar77@users.noreply.github.com>
2022-03-16 12:52:15 +0600

    Add files via upload

---
+++ landing page  2/js/min.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $(".burger-tst").owlCarousel({
+        items:1,
+        nav:false,
+        dots:false,
+        autoplay:false,
+        loop:true,
+        autoplayTimeout:5000,
+        margin:30,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:false
+    
+            }
+        }
+
+    });
+    $(".header-bar").on("click", function(){
+        $(".offcanvas-manu, .offcanvas-overylay").addClass(".active");  
+      });
+      
+      
+      $(".manu-close, .offcanvas-overylay").on("click", function(){
+        $(".offcanvas-manu, .offcanvas-overylay").removeClass(".active");  
+      });
+
+      $(".all_post_main").isotope({
+        layoutMode: 'fitRows',
+      });
+
+
+
+
+
+
+
+    
+  });
